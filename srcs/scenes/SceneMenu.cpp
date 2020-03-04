1cd5aedd93d6eef6061d2b970cab2d0a4e4eb790
tnicolas42 <tnicolas@student.42.fr>
2020-03-04 18:39:49 +0100

    add function to create buttons in SceneMenu

--- srcs/scenes/SceneMenu.cpp
+++ srcs/scenes/SceneMenu.cpp
@@ -5,6 +5,8 @@
 
 #include "SceneMenu.hpp"
 #include "bomberman.hpp"
+#include "Button.hpp"
+#include "Inputs.hpp"
 
 // -- Constructors -------------------------------------------------------------
 
@@ -39,7 +41,6 @@ std::ostream &	operator<<(std::ostream & os, const SceneMenu& my_class) {
 
 // -- Methods ------------------------------------------------------------------
 
-
 /**
  * init game method.
  */
@@ -48,13 +49,41 @@ bool			SceneMenu::init() {
 	return true;
 }
 
-// -- Private Methods ----------------------------------------------------------
-
+/*
+	call this function every frames to update the scene
+*/
 bool	SceneMenu::update(std::chrono::milliseconds last_loop_ms) {
 	(void)last_loop_ms;
+	for (auto it = _buttons.begin(); it != _buttons.end(); it++) {
+		(*it)->update(Inputs::getMousePos(), MouseState::NO_CLICK);
+	}
 	return true;
 }
 
+/*
+	call this function every frames to draw the scene
+*/
 bool	SceneMenu::draw() {
+	for (auto it = _buttons.begin(); it != _buttons.end(); it++) {
+		(*it)->draw();
+	}
 	return true;
 }
+
+/*
+	add a button in the menu
+	sceneobj->addButton(glm::vec2(100, 100), glm::vec2(150, 50), "text in button")
+		->setColor(glm::vec4(0.2, 0.2, 0.8, 1.0));
+*/
+Button *		SceneMenu::addButton(glm::vec2 pos, glm::vec2 size, std::string const & text) {
+	Button * b = new Button(_gui->gameInfo.windowSize, pos, size);
+	b->setText(text);
+	_buttons.push_back(b);
+	return b;
+}
+
+/* getter */
+// get an UI element (button, slider, ...)
+ABaseUI *		SceneMenu::getUIElement(uint32_t id) { return _buttons[id]; }
+// get the total number of UI elements
+uint32_t		SceneMenu::getNbUIElements() const { return _buttons.size(); }
