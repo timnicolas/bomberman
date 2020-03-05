a6999edf272122247be9ccff10bfd934f08b3248
tnicolas42 <tnicolas@student.42.fr>
2020-03-05 13:15:20 +0100

    add slider

---
+++ includes/utils/opengl/UI/Slider.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "includesOpengl.hpp"
+#include "ABaseUI.hpp"
+
+class Slider : public ABaseUI {
+	public:
+		Slider(glm::vec2 winSize, glm::vec2 pos, glm::vec2 size);
+		Slider(Slider const & src);
+		virtual ~Slider();
+
+		Slider & operator=(Slider const & rhs);
+
+		virtual void	draw();
+
+		void			setValues(float min, float max, float val, float step = 0);
+
+	protected:
+		virtual void	_update(glm::vec2 mousePos, bool rightClick, bool leftClick);
+		Slider();
+
+		float _min;
+		float _max;
+		float _val;
+		float _step;
+};
