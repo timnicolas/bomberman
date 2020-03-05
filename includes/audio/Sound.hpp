6f48c065fa3d67f2c73530374ff691c1ba69d7eb
Guilhem Smith <gsmith@student.42.fr>
2020-03-05 16:39:02 +0100

    Exception when soundand music failed to load/play.

--- includes/audio/Sound.hpp
+++ includes/audio/Sound.hpp
@@ -8,6 +8,13 @@
 
 class Sound {
 public:
+	class SoundException: public std::runtime_error {
+		public:
+			SoundException();
+			explicit SoundException(const char* what_arg);
+			explicit SoundException(const std::string what_arg);
+	};
+
 	explicit Sound(std::string filename);
 	~Sound();
 
@@ -17,12 +24,12 @@ public:
 	void										stop();
 	void										updateVolume(float volume, float previous_settings);
 	bool										channelFinished(int chan);
+
 private:
 	Sound();
 	Sound(const Sound &src);
 	Sound										&operator=(const Sound &src);
 
-	std::string									_filename;
 	Mix_Chunk *									_chunk;
 	std::unordered_set<int>						_currents_channels;
 };
