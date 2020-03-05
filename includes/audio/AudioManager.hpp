2758662cefb94bc1c98d87e024ba6779dc820b30
Guilhem Smith <gsmith@student.42.fr>
2020-03-05 11:36:27 +0100

    Sound implemented in AudioManager.

--- includes/audio/AudioManager.hpp
+++ includes/audio/AudioManager.hpp
@@ -14,11 +14,17 @@ public:
 	static AudioManager						&get();
 	static bool								isEnabled();
 	static void								updateSettings();
+
 	static void								loadMusic(std::string filename);
-	static void								loadSound(std::string filename);
 	static void								playMusic();
 	static void								pauseMusic();
-	static void								playSound(std::string sound_name, float volume);
+
+	static void								loadSound(std::string filename);
+	static void								playSound(std::string sound_name, float volume = 1.0);
+	static void								pauseSound(std::string sound_name);
+	static void								resumeSound(std::string sound_name);
+	static void								pauseAllSounds();
+	static void								resumeAllSounds();
 	static void								unloadSound(std::string sound_name);
 	static void								stopAllSounds();
 
@@ -28,13 +34,21 @@ private:
 	AudioManager							&operator=(const AudioManager &src);
 
 	void									_updateSettings();
+
 	void									_loadMusic(std::string filename);
-	void									_loadSound(std::string filename);
 	void									_playMusic();
 	void									_pauseMusic();
+
+	void									_loadSound(std::string filename);
 	void									_playSound(std::string sound_name, float volume);
+	void									_pauseSound(std::string sound_name);
+	void									_resumeSound(std::string sound_name);
+	void									_pauseAllSounds();
+	void									_resumeAllSounds();
 	void									_unloadSound(std::string sound_name);
 	void									_stopAllSounds();
+
+	static void								_channelHalted(int chan);
 
 	float									_volume_master;
 	float									_volume_music;
