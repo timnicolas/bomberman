5ee9d795393b970fe3b9a92159c855f83978daea
Emilien Baudet <emilien.baudet@gmail.com>
2020-03-11 16:45:18 +0100

    [dev] Player movement

--- srcs/elements/Player.cpp
+++ srcs/elements/Player.cpp
@@ -1,4 +1,5 @@
 #include "Player.hpp"
+#include "Inputs.hpp"
 
 // -- Constructors -------------------------------------------------------------
 
@@ -6,6 +7,7 @@ Player::Player(SceneGame &game) : ACharacter(game) {
 	type = Type::PLAYER;
 	bombs = 1;
 	name = "Player";
+	speed = 5;
 }
 
 Player::~Player() {
@@ -35,7 +37,7 @@ Player &Player::operator=(Player const &rhs) {
  * @return false if failure
  */
 bool	Player::update(float const dTime) {
-	(void)dTime;
+	_move(dTime);
 	return true;
 }
 
@@ -48,6 +50,38 @@ bool	Player::update(float const dTime) {
 bool	Player::draw(Gui &gui) {
 	gui.drawCube(Block::PLAYER, getPos());
 	return true;
+}
+
+// -- Private Methods ----------------------------------------------------------
+
+void	Player::_move(float const dTime) {
+	glm::vec3 pos = getPos();
+	if (Inputs::getKey(InputType::UP)) {
+		pos.z -= speed * dTime;
+	}
+	if (Inputs::getKey(InputType::RIGHT)) {
+		pos.x += speed * dTime;
+	}
+	if (Inputs::getKey(InputType::DOWN)) {
+		pos.z += speed * dTime;
+	}
+	if (Inputs::getKey(InputType::LEFT)) {
+		pos.x -= speed * dTime;
+	}
+	if (_canMove(pos))
+		position = pos;
+}
+
+bool	Player::_canMove(glm::vec3 pos) {
+	if (game.board[pos.x + 0.05][pos.z + 0.05].size() == 0
+	&& game.board[pos.x + 0.05][pos.z + 0.95].size() == 0
+	&& game.board[pos.x + 0.95][pos.z + 0.05].size() == 0
+	&& game.board[pos.x + 0.95][pos.z + 0.95].size() == 0) {
+		logDebug("player can move");
+		return true;
+	}
+	logDebug("player cannot move");
+	return false;
 }
 
 // -- Exceptions errors --------------------------------------------------------
