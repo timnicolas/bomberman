8e0ab590c06f9a5d1e2ded1119ca22dc3cc4025a
Guilhem Smith <gsmith@student.42.fr>
2020-03-11 18:41:29 +0100

    Scale text according to the width of the window.

--- includes/scenes/SceneSettings.hpp
+++ includes/scenes/SceneSettings.hpp
@@ -17,55 +17,66 @@ namespace SettingsType {
 
 class SceneSettings: public SceneMenu {
 public:
-	static std::string	audio_name[3];
+	struct				res {
+		int				width;
+		int				height;
+	};
+
+	static const int			nb_resolution = 4;
+	static SceneSettings::res	resolutions[SceneSettings::nb_resolution];
+	static const std::string	audio_name[3];
 
 	virtual ~SceneSettings();
 	explicit SceneSettings(Gui * gui, float const &dtTime);
 	SceneSettings(SceneSettings const &src);
 
-	SceneSettings		&operator=(SceneSettings const &rhs);
+	SceneSettings				&operator=(SceneSettings const &rhs);
 
-	virtual bool		init();
-	virtual bool		update();
+	virtual bool				init();
+	virtual bool				update();
 
 private:
 	SceneSettings();
 
-	void				_saveAudioVolume(int audio_index);
-	void				_updateAudioVolume(int audio_index);
-	void				_updateKey(InputType::Enum key_type);
-	void				_selectPane(SettingsType::Enum pane_type);
-	void				_updateFullscreen();
-	void				_returnQuit();
-	void				_cancelQuit();
+	void						_saveAudioVolume(int audio_index);
+	void						_updateAudioVolume(int audio_index);
+	void						_updateKey(InputType::Enum key_type);
+	void						_selectPane(SettingsType::Enum pane_type);
+	void						_updateFullscreen();
+	void						_returnQuit();
+	void						_cancelQuit();
 
-	void				_init_graphics_pane(glm::vec2 tmp_pos, float menu_width, float menu_height);
-	void				_init_audio_pane(glm::vec2 tmp_pos, float menu_width, float menu_height);
-	void				_init_control_pane(glm::vec2 tmp_pos, float menu_width, float menu_height);
+	void						_init_graphics_pane(glm::vec2 tmp_pos, float menu_width, float menu_height);
+	void						_init_audio_pane(glm::vec2 tmp_pos, float menu_width, float menu_height);
+	void						_init_control_pane(glm::vec2 tmp_pos, float menu_width, float menu_height);
 
-	void				_updateFullscreenButton();
+	void						_updateFullscreenButton();
+	void						_updateResolution();
 
-	int					_input_configuring;
+	float						_text_scale = 1.5f;
+	int							_input_configuring;
+	SceneSettings::res			_selected_resolution;
 
 	/* UI object */
-	SettingsType::Enum	_current_pane;
-	std::list<ABaseUI*>	_panes[SettingsType::nb_types];
-	ButtonUI			*_key_buttons[Inputs::nb_input];
-	ButtonUI			*_fullscreen_button;
+	SettingsType::Enum			_current_pane;
+	std::list<ABaseUI*>			_panes[SettingsType::nb_types];
+	ButtonUI					*_key_buttons[Inputs::nb_input];
+	ButtonUI					*_fullscreen_button;
+	TextUI						*_resolution_text;
 
 	/* temporary settings */
-	bool				_fullscreen;
-	float				_audio_volume[3];
+	bool						_fullscreen;
+	float						_audio_volume[3];
 
 	/* UI listeners */
-	bool				_return;
-	bool				_next_resolution;
-	bool				_prev_resolution;
-	bool				_update_fullscreen;
-	bool				_save_audio[3];
-	float				_update_audio[3];
-	bool				_select_pane[SettingsType::nb_types];
-	bool				_update_key[Inputs::nb_input];
+	bool						_return;
+	bool						_next_resolution;
+	bool						_prev_resolution;
+	bool						_update_fullscreen;
+	bool						_save_audio[3];
+	float						_update_audio[3];
+	bool						_select_pane[SettingsType::nb_types];
+	bool						_update_key[Inputs::nb_input];
 };
 
 #endif
