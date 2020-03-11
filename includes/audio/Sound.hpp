b973b91a0e277da8e6676bf388b06c9942e03938
Guilhem Smith <gsmith@student.42.fr>
2020-03-11 14:09:01 +0100

    Music settings fully customizable from interface.

--- includes/audio/Sound.hpp
+++ includes/audio/Sound.hpp
@@ -5,6 +5,7 @@
 # include <SDL2/SDL_mixer.h>
 # include <string>
 # include <unordered_set>
+# include <map>
 
 class Sound {
 public:
@@ -18,11 +19,11 @@ public:
 	explicit Sound(std::string filename);
 	~Sound();
 
-	void										play(float volume);
+	void										play(float volume, float env_volume);
 	void										pause();
 	void										resume();
 	void										stop();
-	void										updateVolume(float volume, float previous_settings);
+	void										updateVolume(float volume);
 	bool										channelFinished(int chan);
 
 private:
@@ -32,6 +33,7 @@ private:
 
 	Mix_Chunk *									_chunk;
 	std::unordered_set<int>						_currents_channels;
+	std::map<int, float>						_chan_volume;
 };
 
 #endif
