1990b71ffc178a444f08c7ec80c01a71a8ee8c3d
Guilhem Smith <gsmith@student.42.fr>
2020-03-11 10:45:48 +0100

    Input menu configuration done.

--- includes/Inputs.hpp
+++ includes/Inputs.hpp
@@ -44,6 +44,8 @@ public:
 	static bool								getRightClickDown();
 	static bool								getLeftClickDown();
 	static void								update();
+	static std::string						getInputKeyName(InputType::Enum type);
+	static bool								isConfiguring();
 
 private:
 	Inputs();
@@ -65,6 +67,8 @@ private:
 	bool									_getRightClickDown() const;
 	bool									_getLeftClickDown() const;
 	void									_update();
+	std::string								_getInputKeyName(InputType::Enum type);
+	bool									_isConfiguring();
 
 	bool									_configuring;
 	InputType::Enum							_next_action_type;
--- srcs/scenes/SceneSettings.cpp
+++ srcs/scenes/SceneSettings.cpp
@@ -2,6 +2,7 @@
 
 SceneSettings::~SceneSettings() {}
 SceneSettings::SceneSettings(Gui *gui, float const &dtTime) : SceneMenu(gui, dtTime),
+	_input_configuring(-1),
 	_current_pane(SettingsType::GRAPHICS),
 	_next_resolution(false),
 	_prev_resolution(false),
@@ -16,6 +17,7 @@ SceneSettings::SceneSettings(Gui *gui, float const &dtTime) : SceneMenu(gui, dtT
 	}
 	for (auto i = 0; i < Inputs::nb_input; i++) {
 		_update_key[i] = false;
+		_key_buttons[i] = nullptr;
 	}
 	_fullscreen = s.j("graphics").b("fullscreen");
 }
@@ -25,6 +27,7 @@ SceneSettings::SceneSettings(SceneSettings const &src) : SceneMenu(src) {
 
 SceneSettings	&SceneSettings::operator=(SceneSettings const &rhs) {
 	SceneMenu::operator=(rhs);
+	_input_configuring = rhs._input_configuring;
 	_fullscreen = rhs._fullscreen;
 	_current_pane = rhs._current_pane;
 	_next_resolution = rhs._next_resolution;
@@ -39,6 +42,7 @@ SceneSettings	&SceneSettings::operator=(SceneSettings const &rhs) {
 	}
 	for (auto i = 0; i < Inputs::nb_input; i++) {
 		_update_key[i] = rhs._update_key[i];
+		_key_buttons[i] = rhs._key_buttons[i];
 	}
 	return *this;
 }
@@ -88,68 +92,29 @@ void			SceneSettings::_init_control_pane(glm::vec2 tmp_pos, float menu_width, fl
 	glm::vec2 win_size = _gui->gameInfo.windowSize;
 	glm::vec2 tmp_size;
 	ABaseUI *ptr;
+	std::string key_name;
 
 	tmp_size.x = menu_width / 3;
 	tmp_size.y = menu_height * 0.09;
-	tmp_pos.x = (win_size.x / 2) - (menu_width / 2);
-	tmp_pos.y -= menu_height * 0.1;
-	ptr = &addText(tmp_pos, tmp_size, "UP :").setTextAlign(TextAlign::RIGHT);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x += (menu_width / 2);
-	ptr = &addButton(tmp_pos, tmp_size, "key").addButtonLeftListener(&_update_key[InputType::UP]) \
-		.setTextAlign(TextAlign::CENTER);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x = (win_size.x / 2) - (menu_width / 2);
-	tmp_pos.y -= menu_height * 0.1;
-	ptr = &addText(tmp_pos, tmp_size, "DOWN :").setTextAlign(TextAlign::RIGHT);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x += (menu_width / 2);
-	ptr = &addButton(tmp_pos, tmp_size, "key").addButtonLeftListener(&_update_key[InputType::DOWN]) \
-		.setTextAlign(TextAlign::CENTER);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x = (win_size.x / 2) - (menu_width / 2);
-	tmp_pos.y -= menu_height * 0.1;
-	ptr = &addText(tmp_pos, tmp_size, "LEFT :").setTextAlign(TextAlign::RIGHT);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x += (menu_width / 2);
-	ptr = &addButton(tmp_pos, tmp_size, "key").addButtonLeftListener(&_update_key[InputType::LEFT]) \
-		.setTextAlign(TextAlign::CENTER);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x = (win_size.x / 2) - (menu_width / 2);
-	tmp_pos.y -= menu_height * 0.1;
-	ptr = &addText(tmp_pos, tmp_size, "RIGHT :").setTextAlign(TextAlign::RIGHT);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x += (menu_width / 2);
-	ptr = &addButton(tmp_pos, tmp_size, "key").addButtonLeftListener(&_update_key[InputType::RIGHT]) \
-		.setTextAlign(TextAlign::CENTER);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x = (win_size.x / 2) - (menu_width / 2);
-	tmp_pos.y -= menu_height * 0.1;
-	ptr = &addText(tmp_pos, tmp_size, "ACTION :").setTextAlign(TextAlign::RIGHT);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x += (menu_width / 2);
-	ptr = &addButton(tmp_pos, tmp_size, "key").addButtonLeftListener(&_update_key[InputType::ACTION]) \
-		.setTextAlign(TextAlign::CENTER);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x = (win_size.x / 2) - (menu_width / 2);
-	tmp_pos.y -= menu_height * 0.1;
-	ptr = &addText(tmp_pos, tmp_size, "CONFIRM :").setTextAlign(TextAlign::RIGHT);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x += (menu_width / 2);
-	ptr = &addButton(tmp_pos, tmp_size, "key").addButtonLeftListener(&_update_key[InputType::CONFIRM]) \
-		.setTextAlign(TextAlign::CENTER);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x = (win_size.x / 2) - (menu_width / 2);
-	tmp_pos.y -= menu_height * 0.1;
-	ptr = &addText(tmp_pos, tmp_size, "CANCEL :").setTextAlign(TextAlign::RIGHT);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
-	tmp_pos.x += (menu_width / 2);
-	ptr = &addButton(tmp_pos, tmp_size, "key").addButtonLeftListener(&_update_key[InputType::CANCEL]) \
-		.setTextAlign(TextAlign::CENTER);
-	_panes[SettingsType::CONTROLS].push_front(ptr);
+	for (auto i = 0; i < Inputs::nb_input; i++) {
+		tmp_pos.x = (win_size.x / 2) - (menu_width / 2);
+		tmp_pos.y -= menu_height * 0.1;
+		ptr = &addText(tmp_pos, tmp_size, Inputs::input_type_name[i] + " :").setTextAlign(TextAlign::RIGHT);
+		_panes[SettingsType::CONTROLS].push_front(ptr);
+		tmp_pos.x += (menu_width / 2);
+		key_name = Inputs::getInputKeyName(static_cast<InputType::Enum>(i));
+		ptr = &addButton(tmp_pos, tmp_size, key_name).addButtonLeftListener(&_update_key[i]) \
+			.setTextAlign(TextAlign::CENTER);
+		_panes[SettingsType::CONTROLS].push_front(ptr);
+		_key_buttons[i] = reinterpret_cast<ButtonUI*>(ptr);
+	}
 }
 bool			SceneSettings::update() {
 	SceneMenu::update();
+	if (_input_configuring >= 0 && !Inputs::isConfiguring()) {
+		_key_buttons[_input_configuring]->setText(Inputs::getInputKeyName(static_cast<InputType::Enum>(_input_configuring)));
+		_input_configuring = -1;
+	}
 	for (auto i = 0; i < SettingsType::nb_types; i++) {
 		if (_select_pane[i]) {
 			_selectPane(static_cast<SettingsType::Enum>(i));
@@ -182,6 +147,8 @@ void			SceneSettings::_updateKey(InputType::Enum key_type) {
 	_update_key[key_type] = false;
 	// TODO(gsmith): add button to cancel key configuration ?
 	logInfo("Start configuring key...");
+	_key_buttons[key_type]->setText("...");
+	_input_configuring = key_type;
 	Inputs::configureKey(key_type);
 }
 
