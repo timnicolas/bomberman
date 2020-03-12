89a60d6a83f8887e6475180130ad82cfe7015f6c
tnicolas42 <tnicolas@student.42.fr>
2020-03-12 11:36:05 +0100

    close #42 add option in ABaseUI -> add enable/disable option

--- srcs/utils/opengl/UI/ImageUI.cpp
+++ srcs/utils/opengl/UI/ImageUI.cpp
@@ -39,7 +39,7 @@ void ImageUI::_update() {
  * @brief this is the draw function for UI
  * /!\ -> you need to draw in the reverse order (draw at first the element on the top)
  */
-void ImageUI::draw() {
+void ImageUI::_draw() {
 	glm::vec2 tmpPos;
 	glm::vec2 tmpSize;
 
