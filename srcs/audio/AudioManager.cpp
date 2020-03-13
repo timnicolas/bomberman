b311ae16e48169cc089f94dedab068f9b8906cf5
zer0nim <emarin@student.42.fr>
2020-03-13 15:53:24 +0100

    use alloc-dealloc-mismatch (malloc vs operator delete) on getcwd

--- srcs/audio/AudioManager.cpp
+++ srcs/audio/AudioManager.cpp
@@ -12,7 +12,7 @@ AudioManager::AudioManager(): _music_modifier(1.0) {
 	char	*path = getcwd(NULL, PATH_MAX);
 	AudioManager::_assets_path = std::string(path);
 	AudioManager::_assets_path += "/bomberman-assets/";
-	delete path;
+	free(path);
 	logDebug("Assets_path: " << AudioManager::_assets_path);
 	if (SDL_Init(SDL_INIT_AUDIO) < 0) {
 		_enabled = false;
