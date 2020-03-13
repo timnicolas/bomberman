7381bcf2d54adb45ee777e732d2371b395902789
Guilhem Smith <smith.guilhem@gmail.com>
2020-03-13 15:31:31 +0100

    Compensate the new break instruction.

--- srcs/scenes/SceneGame.cpp
+++ srcs/scenes/SceneGame.cpp
@@ -346,6 +346,7 @@ bool	SceneGame::_loadLevel(int32_t levelId) {
 						break;
 					case EntityType::BOARD_FLAG:
 						flags++;
+						board[i][j].push_back(entity);
 						break;
 					case EntityType::BOARD:
 						board[i][j].push_back(entity);
