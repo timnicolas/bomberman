9511b2178ba20e04e2cc1c5b3fe55db0c393cee3
tnicolas42 <tnicolas@student.42.fr>
2020-03-23 16:44:31 +0100

    add mkdir, rm, isDir functions

--- includes/utils/FileUtils.hpp
+++ includes/utils/FileUtils.hpp
@@ -4,4 +4,8 @@
 
 namespace file {
 	bool	isFile(std::string const & filename);
+	bool	isDir(std::string const & path);
+
+	bool	mkdir(std::string const & path, bool silent = false);
+	bool rm(std::string const & path, bool silent = false);
 }
--- srcs/utils/FileUtils.cpp
+++ srcs/utils/FileUtils.cpp
@@ -1,5 +1,8 @@
+#include <filesystem>
 #include <fstream>
+#include <iostream>
 #include "FileUtils.hpp"
+#include "Logging.hpp"
 
 namespace file {
 	/**
@@ -9,9 +12,71 @@ namespace file {
 	 * @return true If the file exist
 	 */
 	bool isFile(std::string const & filename) {
-		std::ifstream ifile(filename);
-		if (ifile)
-			return true;
-		return false;
+		return std::filesystem::is_regular_file(filename);
 	}
+
+	/**
+	 * @brief Try if it's a directory
+	 *
+	 * @param path The directory path
+	 * @return true If the directory exist
+	 */
+	bool	isDir(std::string const & path) {
+		return std::filesystem::is_directory(path);
+	}
+
+	/**
+	 * @brief Create a directory
+	 *
+	 * @param path The path of the directory
+	 * @param silent If true, don't show warning and errors
+	 * @return false if failed to create directories
+	 */
+	bool	mkdir(std::string const & path, bool silent) {
+		bool		error = false;
+		std::string	msg = "";
+		try {
+			std::filesystem::create_directories(path);
+			if (file::isDir(path) == false)
+				error = true;
+		}
+		catch (std::exception const & e) {
+			error = true;
+			msg = std::string(" -> ") + e.what();
+		}
+		if (error) {
+			if (!silent)
+				logErr("Unable to create dir: " << path << msg);
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @brief Remove a file or a directory
+	 *
+	 * @param path The path to remove
+	 * @param silent If true, don't show warning and errors
+	 * @return false if failed to remove directory
+	 */
+	bool rm(std::string const & path, bool silent) {
+		bool		error = false;
+		std::string	msg = "";
+		try {
+			std::filesystem::remove_all(path);
+			if (file::isDir(path))
+				error = true;
+		}
+		catch (std::exception const & e) {
+			error = true;
+			msg = std::string(" -> ") + e.what();
+		}
+		if (error) {
+			if (!silent)
+				logErr("Unable to create dir: " << path << msg);
+			return false;
+		}
+		return true;
+	}
+
 }  // namespace file
