133d643e56a61afaa83c6415d368dc2ddf61484c
zer0nim <emarin@student.42.fr>
2020-03-27 00:08:26 +0100

    try to render player animated model

--- srcs/scenes/SceneGame.cpp
+++ srcs/scenes/SceneGame.cpp
@@ -76,6 +76,9 @@ SceneGame::~SceneGame() {
 		delete *it;
 	}
 	_mapsList.clear();
+
+	delete _model;
+	delete _openGLModel;
 }
 
 SceneGame::SceneGame(SceneGame const &src)
@@ -129,6 +132,20 @@ bool			SceneGame::init() {
 			break;
 		}
 		i++;
+	}
+
+	try {
+		_openGLModel = new OpenGLModel(*_gui->cam, "bomberman-assets/3dModels/"
+			"white/white_with_anims.fbx", {0, -0.5, 0});
+		_model = new Model(*_openGLModel, _dtTime, ETransform({1, 0, 1}));
+		_model->play = true;
+		_model->loopAnimation = true;
+
+		_model->setAnimation("Armature|idle");
+	}
+	catch(OpenGLModel::ModelException const & e) {
+		logErr(e.what());
+		return false;
 	}
 
 	return true;
@@ -291,6 +308,18 @@ bool	SceneGame::draw() {
 	_gui->cubeShader->unuse();
 
 	ASceneMenu::draw();
+
+	try {
+		if (Inputs::getKeyByScancodeDown(SDL_SCANCODE_1)) {
+			_model->setNextAnimation();
+		}
+
+		_model->draw();
+	}
+	catch(OpenGLModel::ModelException const & e) {
+		logErr(e.what());
+		return false;
+	}
 
 	// draw skybox
 	_gui->drawSkybox(view);
