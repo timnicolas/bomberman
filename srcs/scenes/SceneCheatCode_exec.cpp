7f942d438f8d59169c6d658fa4522227118eb5b3
tnicolas42 <tnicolas@student.42.fr>
2020-04-03 13:59:02 +0200

    add log command in command line

--- srcs/scenes/SceneCheatCode_exec.cpp
+++ srcs/scenes/SceneCheatCode_exec.cpp
@@ -2,7 +2,6 @@
 #include "SceneGame.hpp"
 
 bool SceneCheatCode::_execHelp(std::vector<std::string> const & args) {
-	(void)args;
 	if (args.size() == 1) {  // only /help
 		_addLine("List of all commands:");
 		std::string commands = CHEATCODE_TAB;
@@ -52,5 +51,30 @@ bool SceneCheatCode::_execClear(std::vector<std::string> const & args) {
 	else {
 		clearAllLn();
 	}
+	return true;  // exit command line after this
+}
+
+bool SceneCheatCode::_execLog(std::vector<std::string> const & args) {
+	if (args.size() == 3) {
+		if (args[1] == "debug")
+			this->logdebug(args[2], false, true);
+		else if (args[1] == "info")
+			this->loginfo(args[2], false, true);
+		else if (args[1] == "success")
+			this->logsuccess(args[2], false, true);
+		else if (args[1] == "warn")
+			this->logwarn(args[2], false, true);
+		else if (args[1] == "err")
+			this->logerr(args[2], false, true);
+		else if (args[1] == "fatal")
+			this->logfatal(args[2], false, true);
+		else
+			this->logerr("invalid log type: " + args[1] + " (/help " + args[0] + ")", false, true);
+	}
+	else {
+		_execHelp({"help", "log"});
+		return false;
+	}
+	SceneManager::openCheatCodeForTime(s.j("cheatcode").u("timeLineShow"));  // show lines for x seconds
 	return true;  // exit command line after this
 }
