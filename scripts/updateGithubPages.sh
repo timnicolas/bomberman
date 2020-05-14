d5e0b1199c5677a3d0bde33bf83b32c498b9d697
tnicolas42 <tnicolas@student.42.fr>
2020-05-14 17:03:02 +0200

    update script for documentation

--- scripts/updateGithubPages.sh
+++ scripts/updateGithubPages.sh
@@ -5,11 +5,13 @@ TMP_DIR="/tmp/__code_docs"
 DOXYFILE="scripts/Doxyfile"
 
 rm -rf $TMP_DIR
-git clone -b gh-pages GIT_REPO_URL $TMP_DIR
+git clone -b gh-pages $GIT_REPO_URL $TMP_DIR
 
 doxygen $DOXYFILE
 
 cd $TMP_DIR
 git add -A
-git commit --amend -m "update docs" --no-verify
+git commit -m "update docs" --no-verify
 git push origin gh-pages -f
+
+rm -rf $TMP_DIR
