354e60773469a0d7a180543c9edf4eb7aa5fe391
Emilien Baudet <emilien.baudet@gmail.com>
2020-05-24 01:49:22 +0200

    theme fix : change border position + opacity

--- srcs/bomberman.cpp
+++ srcs/bomberman.cpp
@@ -144,7 +144,7 @@ bool	initSettings(std::string const & filename) {
 		s.j("colors").j("background").add<uint64_t>("alpha", 0xFF).setMin(0x00).setMax(0xFF);
 	s.j("colors").add<SettingsJson>("bg-rect");
 		s.j("colors").j("bg-rect").add<uint64_t>("color", 0xf8f4ef).setMin(0x000000).setMax(0xFFFFFF);
-		s.j("colors").j("bg-rect").add<uint64_t>("alpha", 0x44).setMin(0x00).setMax(0xFF);
+		s.j("colors").j("bg-rect").add<uint64_t>("alpha", 0x55).setMin(0x00).setMax(0xFF);
 	s.j("colors").add<SettingsJson>("bg-rect-border");
 		s.j("colors").j("bg-rect-border").add<uint64_t>("color", 0xCF647A).setMin(0x000000).setMax(0xFFFFFF);
 		s.j("colors").j("bg-rect-border").add<uint64_t>("alpha", 0xFF).setMin(0x00).setMax(0xFF);
