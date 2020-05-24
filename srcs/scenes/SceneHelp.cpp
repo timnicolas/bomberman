c7467da6e93bc1efb582fecf22e37fa4dd235905
tnicolas42 <tnicolas@student.42.fr>
2020-05-24 17:32:13 +0200

    move text for bonus explanation (in scene help)

--- srcs/scenes/SceneHelp.cpp
+++ srcs/scenes/SceneHelp.cpp
@@ -159,7 +159,7 @@ bool			SceneHelp::init() {
 			if (desc.rfind("Bonus ", 0) == 0) {
 				desc.erase(0, strlen("Bonus "));
 			}
-			tmpUI = &addText({tmpPos.x + 35, tmpPos.y}, {menuWidth, imgSize.y}, desc)
+			tmpUI = &addText({tmpPos.x + imgSize.x + 5, tmpPos.y}, {menuWidth, imgSize.y}, desc)
 				.setTextFont("text")
 				.setTextScale(HELP_MENU_TEXT_SCALE)
 				.setTextAlign(TextAlign::LEFT);
